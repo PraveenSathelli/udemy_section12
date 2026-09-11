8898eb8f74eee2adb156ea74ff3aaec8d4c8c502
PraveenSathelli <praveensathelli11@gmail.com>
2026-09-11 12:24:51 +0530

    rxjs based error

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -26,14 +26,19 @@ export class AvailablePlacesComponent implements OnInit {
     const subscribtion = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').pipe(
       map((response) => {
         return response.places;
+      }),
+      catchError((error) => {
+        console.log("error: " + error);
+        return throwError(() => {
+          new Error("error from places");
+        })
       })
     ).subscribe({
       next: (data) => {
         console.log(data)
         this.places.set(data)
-      }, error: (err) => {
-        console.error(err.message)
-        this.error.set("SOmething went wrong..")
+      }, error: (err:Error) => {
+        this.error.set(err.message);
       }, complete: () => {
         //we can use set but just want to check this :) 
         this.isFetching.update((oldValue) => oldValue ? false : true);
