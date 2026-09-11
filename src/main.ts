034d5e2649cfd5774be3b7a2f5ce5b78b1686cbe
PraveenSathelli <praveensathelli11@gmail.com>
2026-09-11 22:21:18 +0530

    latest intercept code change per latest standards

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
+function requestLog(request: HttpRequest<unknown>, next: HttpHandlerFn) {
 
-bootstrapApplication(AppComponent,{providers:[provideHttpClient()]}).catch((err) => console.error(err));
+    console.log("intercepted request " + request.url);
+    let updateRequest = request.headers.set('HTTP-DEB', 'Testing');
+    return next(request);
+}
+
+bootstrapApplication(AppComponent, { providers: [provideHttpClient(withInterceptors([requestLog]))] }).catch((err) => console.error(err));
