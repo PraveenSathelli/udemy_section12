3ed4566c575b97e70d46f566b54d29cabf28c3a0
PraveenSathelli <praveensathelli11@gmail.com>
2026-09-11 22:33:12 +0530

    reponse intercepter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function requestLog(request: HttpRequest<unknown>, next: HttpHandlerFn) {
 
     console.log("intercepted request " + request.url);
     let updateRequest = request.headers.set('HTTP-DEB', 'Testing');
-    return next(request);
+    return next(request).pipe(
+        tap(next => {
+            if (next.type === HttpEventType.Response) {
+                console.log("response " + next.status);
+            }
+        })
+    );
 }
 
 bootstrapApplication(AppComponent, { providers: [provideHttpClient(withInterceptors([requestLog]))] }).catch((err) => console.error(err));
